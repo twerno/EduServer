fc747609144c7ac0a795f196bc487241e9a549fa
Tomek <twerno@gmail.com>
2011-08-19 14:28:54 +0200

    bledy w testach

--- src/test/java/net/twerno/eduserver/user/domain/AccountDataOnDemand.java
+++
@@ -1,9 +0,0 @@
-package net.twerno.eduserver.user.domain;
-
-import net.twerno.eduserver.user.entities.Account;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Account.class)
-public class AccountDataOnDemand {
-}
